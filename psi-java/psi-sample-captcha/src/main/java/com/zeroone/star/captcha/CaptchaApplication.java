234b10a39dbabacaa479e6e1d82131a0a55af0fb
awei <2069682479@qq.com>
2023-02-06 20:09:19 +0800

    新增验证码插件后端实现示例模块

--- psi-java/psi-sample-captcha/src/main/java/com/zeroone/star/captcha/CaptchaApplication.java
+++ psi-java/psi-sample-captcha/src/main/java/com/zeroone/star/captcha/CaptchaApplication.java
@@ -1,4 +1,4 @@
-package com.zeroone.star.sample;
+package com.zeroone.star.captcha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,17 +6,20 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * <p>
- * 描述：程序入口
+ * 描述：服务器启动入口
  * </p>
  * <p>版权：&copy;01星球</p>
  * <p>地址：01星球总部</p>
+ *
  * @author 阿伟学长
  * @version 1.0.0
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-public class Application {
+public class CaptchaApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
+        SpringApplication.run(CaptchaApplication.class, args);
     }
+
 }
