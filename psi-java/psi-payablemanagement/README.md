e789a0979f9b8e82e781a3b8b517c1ad428e24f4
iceberg-work <iceberg-work@qq.com>
2023-02-08 16:17:07 +0800

    应付管理模块初始化

---
+++ psi-java/psi-payablemanagement/README.md
@@ -0,0 +1,11 @@
+# 工程简介
+
+## 模块名
+
+* English
+  * PayableManagement
+* Chinese
+  * 应付管理
+  
+# 延伸阅读
+
---
+++ psi-java/psi-payablemanagement/src/main/java/com/zeroone/star/payablemanagement/controller/SampleController.java
@@ -0,0 +1,56 @@
+package com.zeroone.star.payablemanagement.controller;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.zeroone.star.project.vo.JsonVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.Data;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用例
+ *
+ * @author iceberg-work
+ */
+@Controller
+@RequestMapping("sample")
+@Api(tags = "用例")
+@RestController
+public class SampleController {
+
+    @ApiOperation(value = "获取用例1")
+    @GetMapping(value = "get-sample1")
+    public JsonVO<User> getSample1(User user) {
+        System.out.println(user);
+        System.out.println(JsonVO.success(user));
+        return JsonVO.success(user);
+    }
+
+    /**
+     * 加了@RequestBody后要传json格式数据
+     */
+    @ApiOperation(value = "获取用例2")
+    @GetMapping(value = "get-sample2")
+    public JsonVO<User> getSample2(@RequestBody User user) {
+        System.out.println(user);
+        System.out.println(JsonVO.success(user));
+        return JsonVO.success(user);
+    }
+}
+
+@Data
+
+class User {
+    /** value表示该属性对应的表头名称， index表示该属性所处的列的位置 */
+    @ExcelProperty(value = "编号", index = 0)
+    private Integer id;
+    @ExcelProperty(value = "姓名", index = 1)
+    private String name;
+    @ExcelProperty(value = "电话", index = 2)
+    private String phone;
+}
