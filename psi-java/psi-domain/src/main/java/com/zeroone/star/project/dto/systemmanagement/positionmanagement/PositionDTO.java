747967d2b5a6517efb1ada108716650c0bd2ceba
Your-Pluviophile <935664910@qq.com>
2023-02-11 16:20:09 +0800

    完善职务管理DTO（增删改）

--- psi-java/psi-domain/src/main/java/com/zeroone/star/project/dto/systemmanagement/positionmanagement/PositionDTO.java
+++ psi-java/psi-domain/src/main/java/com/zeroone/star/project/dto/systemmanagement/positionmanagement/PositionDTO.java
@@ -5,17 +5,23 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
- * @Description add / update
+ * @Description add / update /delete
  * @Author mumu
  * @Data 2023-02-11 15:57
  */
 @Data
-@ApiModel("职务管理传输对象")
+@ApiModel("添加/编辑/删除职务DTO")
 public class PositionDTO {
+
+    @ApiModelProperty(value = "职务对象ID",example = "1624315388342083586")
+    private Integer Id;
+
     @ApiModelProperty(value = "职务编码", example = "devleader")
     private Integer code;
+
     @ApiModelProperty(value = "职务名称", example = "研发部经理")
     private String name;
+
     @ApiModelProperty(value = "职级", example = "2")
     private String postRank;
 
