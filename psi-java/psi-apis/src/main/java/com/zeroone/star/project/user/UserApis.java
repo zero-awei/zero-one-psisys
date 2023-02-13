8eaa3809d5506e008db1d8f5086699dfe55e0356
李嘉鑫 <1509230273@qq.com>
2023-02-13 21:43:05 +0800

    总算是见到了接口文档的界面了

--- psi-java/psi-apis/src/main/java/com/zeroone/star/project/user/UserApis.java
+++ psi-java/psi-apis/src/main/java/com/zeroone/star/project/user/UserApis.java
@@ -1,4 +1,79 @@
 package com.zeroone.star.project.user;
 
+import com.zeroone.star.project.dto.user.UserDTO;
+import com.zeroone.star.project.query.user.UserQuery;
+import com.zeroone.star.project.vo.JsonVO;
+import com.zeroone.star.project.vo.PageVO;
+import com.zeroone.star.project.vo.user.UserVO;
+
+import java.util.List;
+
 public interface UserApis {
+    /**
+     * 用户列表
+     * @param query 查询条件
+     * @return 查询结果
+     */
+    JsonVO<PageVO<UserVO>> listAllUsers(UserQuery query);
+
+    /**
+     * 查询用户
+     * @param query 查询条件
+     * @return 查询结果
+     */
+    JsonVO<PageVO<UserVO>> queryUser(UserQuery query);
+
+    /**
+     * 数据回显
+     * @param id 用户id
+     * @return 用户信息
+     */
+    JsonVO<UserVO> review(String id);
+
+    /**
+     * 新增用户
+     * @param dto 数据对象
+     * @return 新增用户的编号
+     */
+    JsonVO<String> addUser(UserDTO dto);
+
+    /**
+     * 删除用户
+     * @param id 用户id
+     * @return 删除用户的编号
+     */
+    JsonVO<String> deleteUser(String id);
+
+    /**
+     * 修改用户
+     * @param dto 数据数据内容
+     * @return 修改后的用户编号
+     */
+    JsonVO<String> modifyUser(UserDTO dto);
+
+    /**
+     * 冻结/解冻用户
+     * @param id 用户id
+     * @return 修改的用户编号
+     */
+    JsonVO<String> modifyStatus(String id);
+
+    /**
+     * 用户导入
+     * @return
+     */
+    JsonVO<String> upload();
+
+    /**
+     * 导出选定用户
+     * @param id 用户id
+     * @return 下载地址
+     */
+    JsonVO<String> download(List<String> id);
+
+    /**
+     * 导出全部用户
+     * @return 下载地址
+     */
+    JsonVO<String> downloadAllUsers();
 }
--- psi-java/psi-user/src/main/java/com/zeroone/star/user/controller/UserController.java
+++ psi-java/psi-user/src/main/java/com/zeroone/star/user/controller/UserController.java
@@ -1,21 +1,95 @@
 package com.zeroone.star.user.controller;
 
+import com.zeroone.star.project.dto.user.UserDTO;
+import com.zeroone.star.project.query.user.UserQuery;
+import com.zeroone.star.project.user.UserApis;
+import com.zeroone.star.project.vo.JsonVO;
+import com.zeroone.star.project.vo.PageVO;
+import com.zeroone.star.project.vo.user.UserVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * <p>
- * 用户表 前端控制器
- * </p>
- *
- * @author axin
- * @since 2023-02-12
- */
 @RestController
-@RequestMapping("/user/sys-user")
-public class UserController {
+@RequestMapping("/user")
+@Api(tags = "用户列表示例接口")
+//@Validated
+public class UserController implements UserApis {
 
+    @ApiOperation(value = "查询用户列表")
+    @GetMapping("list-all")
+    @Override
+    public JsonVO<PageVO<UserVO>> listAllUsers(UserQuery query) {
+        return null;
+    }
+
+    @ApiOperation(value = "查询用户")
+    @GetMapping("query-user")
+    @Override
+    public JsonVO<PageVO<UserVO>> queryUser(UserQuery query) {
+        return null;
+    }
+
+    @Override
+    public JsonVO<UserVO> review(String id) {
+        return null;
+    }
+
+    @ApiOperation(value = "用户信息回显")
+    @GetMapping("echo")
+    public JsonVO<UserVO> echo(String id) {
+        return null;
+    }
+
+
+    @ApiOperation(value = "添加用户")
+    @PostMapping("add")
+    @Override
+    public JsonVO<String> addUser(UserDTO dto) {
+        return null;
+    }
+
+    @ApiOperation(value = "删除用户")
+    @DeleteMapping("delete")
+    @Override
+    public JsonVO<String> deleteUser(String id) {
+        return null;
+    }
+
+    @ApiOperation(value = "修改用户")
+    @PutMapping("modify")
+    @Override
+    public JsonVO<String> modifyUser(UserDTO dto) {
+        return null;
+    }
+
+    @ApiOperation(value = "冻结/解冻用户")
+    @PutMapping("status")
+    @Override
+    public JsonVO<String> modifyStatus(String id) {
+        return null;
+    }
+
+    @ApiOperation(value = "导入用户")
+    @GetMapping("upload")
+    @Override
+    public JsonVO<String> upload() {
+        return null;
+    }
+
+    @ApiOperation(value = "导出用户")
+    @GetMapping(value = "get-user")
+    @Override
+    public JsonVO<String> download(List<String> id) {
+        return null;
+    }
+
+    @ApiOperation(value = "导出所有用户")
+    @GetMapping(value = "get-users")
+    @Override
+    public JsonVO<String> downloadAllUsers() {
+        return null;
+    }
 }
-
