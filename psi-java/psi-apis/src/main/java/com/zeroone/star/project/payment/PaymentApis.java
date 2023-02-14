c5ea15545bb1f9c95fdbac412a8d642023b13caf
Huo <1653566469@qq.com>
2023-02-14 14:42:02 +0800

    删除了自定义apis，修改到了模块apis下

--- psi-java/psi-apis/src/main/java/com/zeroone/star/project/payment/PaymentApis.java
+++ psi-java/psi-apis/src/main/java/com/zeroone/star/project/payment/PaymentApis.java
@@ -1,5 +1,10 @@
 package com.zeroone.star.project.payment;
 
+import com.zeroone.star.project.query.payment.PreDetQuery;
+import com.zeroone.star.project.vo.JsonVO;
+import com.zeroone.star.project.vo.payment.DetHavVO;
+import com.zeroone.star.project.vo.payment.DetNoVO;
+
 /**
  * <p>
  * 描述：API接口定义
@@ -11,4 +16,17 @@ package com.zeroone.star.project.payment;
  * @version 1.0.0
  */
 public interface PaymentApis {
+    /**
+     * 通过单据编号查询数据-采购预付有申请
+     * @param condition 单据编号-查询条件
+     * @return 查询结果
+     */
+    JsonVO<DetHavVO> queryByBillHav(PreDetQuery condition);
+
+    /**
+     *通过单据编号查询数据-采购预付无申请
+     * @param condition 单据编号-查询条件
+     * @return 查询结果
+     */
+    JsonVO<DetNoVO> queryByBillNo (PreDetQuery condition);
 }
--- psi-java/psi-payment/src/main/java/com/zeroone/star/payment/controller/PaymentController.java
+++ psi-java/psi-payment/src/main/java/com/zeroone/star/payment/controller/PaymentController.java
@@ -1,4 +1,33 @@
 package com.zeroone.star.payment.controller;
 
-public class PaymentController {
+import com.zeroone.star.project.payment.PaymentApis;
+import com.zeroone.star.project.query.payment.PreDetQuery;
+import com.zeroone.star.project.vo.JsonVO;
+import com.zeroone.star.project.vo.payment.DetHavVO;
+import com.zeroone.star.project.vo.payment.DetNoVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment")
+@Api(tags = "查询详细信息")
+@Validated
+public class PaymentController implements PaymentApis {
+    @GetMapping("/detHav")
+    @ApiOperation(value = "有申请详情信息")
+    @Override
+    public JsonVO<DetHavVO> queryByBillHav(PreDetQuery condition) {
+        return JsonVO.success(new DetHavVO());
+    }
+
+    @GetMapping("/detNo")
+    @ApiOperation(value = "无申请详情信息")
+    @Override
+    public JsonVO<DetNoVO> queryByBillNo(PreDetQuery condition) {
+        return JsonVO.success(new DetNoVO());
+    }
 }
