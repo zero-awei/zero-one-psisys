d31f86f6a7f26bbfb5b7be5a4cf1ce9f14d4ba83
zhd <1106126227@qq.com>
2023-02-14 15:44:30 +0800

    修改接口

--- psi-java/psi-payment/src/main/java/com/zeroone/star/payment/controller/PaymentController.java
+++ psi-java/psi-payment/src/main/java/com/zeroone/star/payment/controller/PaymentController.java
@@ -1,4 +1,34 @@
 package com.zeroone.star.payment.controller;
 
-public class PaymentController {
+import com.zeroone.star.project.payment.PaymentApis;
+import com.zeroone.star.project.query.payment.DocListQuery;
+import com.zeroone.star.project.vo.JsonVO;
+import com.zeroone.star.project.vo.PageVO;
+import com.zeroone.star.project.vo.payment.DocListVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment")
+@Api(tags = "单据查询")
+@Validated
+public class PaymentController implements PaymentApis {
+    /**
+     * <p>
+     * 单据查询 前端控制器
+     * </p>
+     *
+     * @author husj
+     * @since 2023-02-13
+     */
+    @Override
+    @GetMapping("/condition")
+    @ApiOperation(value = "按照条件实现单据查询")
+    public JsonVO<PageVO<DocListVO>> queryAll(DocListQuery condition) {
+        return null;
+    }
 }
