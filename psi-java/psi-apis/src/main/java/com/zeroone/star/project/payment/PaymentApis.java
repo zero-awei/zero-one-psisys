cda369617cb8a747a88b945566c8a2b8aa2a3fff
zhd <1106126227@qq.com>
2023-02-14 15:46:08 +0800

    修改接口

--- psi-java/psi-apis/src/main/java/com/zeroone/star/project/payment/PaymentApis.java
+++ psi-java/psi-apis/src/main/java/com/zeroone/star/project/payment/PaymentApis.java
@@ -4,20 +4,14 @@ import com.zeroone.star.project.dto.payment.ModifyDTO;
 import com.zeroone.star.project.vo.JsonVO;
 
 /**
- * <p>
  * 描述：测试API接口
- * </p>
- * <p>版权：&copy;01星球</p>
- * <p>地址：01星球总部</p>
- *
- * @author forever爱
- * @version 1.0.0
  */
 public interface PaymentApis {
     /**
      * 修改采购预付单功能
-     * @param modifyDTO 带明细表的收款单DTO
-     * @return 查询结果
+     * param modifyDTO 带明细表的收款单DTO
+     * return 查询结果
+     * author forever爱
      */
     JsonVO<String> modifyById(ModifyDTO modifyDTO);
 
