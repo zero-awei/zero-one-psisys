3c420aa4e87f5fcfe38ce1652843388d934f75e7
alun187 <482911313@qq.com>
2023-02-15 01:19:28 +0800

    实例项目并创建文件夹

--- psi-java/psi-accountPayable/src/main/java/com/zeroone/star/payable/AccountPayable.java
+++ psi-java/psi-accountPayable/src/main/java/com/zeroone/star/payable/AccountPayable.java
@@ -3,7 +3,6 @@ package com.zeroone.star.payable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * <p>
@@ -16,7 +15,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 public class AccountPayable {
     public static void main(String[] args) {
         SpringApplication.run(AccountPayable.class, args);
