65a6bb2576eb82b97e4a6a8bc6005de18990ce34
thirduncle <2284027823@qq.com>
2023-02-14 20:00:41 +0800

    第一次整改

---
+++ psi-java/psi-basedetail/src/main/java/com/zeroone/star/basedetail/config/ComponentScanInit.java
@@ -0,0 +1,18 @@
+package com.zeroone.star.basedetail.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 扫描自定义组件
+ * @author Third uncle
+ * @date 2023/2/14 - 15:57
+ */
+@Configuration
+@ComponentScan({
+    "com.zeroone.star.project.components.fastdfs",
+        "com.zeroone.star.project.components.easyexcel"
+})
+public class ComponentScanInit {
+
+}
--- psi-java/psi-titlepage/src/main/java/com/zeroone/star/titlepage/controller/HomepageController.java
+++ psi-java/psi-titlepage/src/main/java/com/zeroone/star/titlepage/controller/HomepageController.java
@@ -24,6 +24,14 @@ import java.util.List;
 @Api(tags = "首页数据查询")
 public class HomepageController implements HomepageApis {
     @Autowired
+    private IPurOrderService purOrderService;
+    @Autowired
+    private IStkIoSumService stkIoSumService;
+    @Autowired
+    private IBasSupplierService basSupplierService;
+    @Autowired
+    private HomeDoingBillServiceImpl hdbservice;
+    @Autowired
     private ISalOrderService salOrderService;
     @ApiOperation("查询毛利润")
     @GetMapping("/querySalOrdel")
@@ -31,37 +39,28 @@ public class HomepageController implements HomepageApis {
     public JsonVO<List<SalOrderVO>> querySalOrder() {
         return JsonVO.success(salOrderService.listSalOrder());
     }
-    @Autowired
-    private IPurOrderService purOrderService;
     @ApiOperation("查询采购金额")
     @GetMapping("/queryPurOrder")
     @Override
     public JsonVO<List<PurOrderVO>> queryPurOrder() {
         return JsonVO.success(purOrderService.queryPurOrder());
     }
-    @Autowired
-    private IStkIoSumService stkIoSumService;
     @ApiOperation("查询库存结存金额")
     @GetMapping("/queryStkIoSum")
     @Override
     public JsonVO<List<StkIoSumVO>> queryStkIoSum() {
         return JsonVO.success(stkIoSumService.queryStkIoSum());
     }
-    @Autowired
-    private IBasSupplierService basSupplierService;
     @ApiOperation("查询供应商")
     @GetMapping("/queryBasSupplier")
     @Override
     public JsonVO<List<BasSupplierVO>> queryBasSupplier() {
         return JsonVO.success(basSupplierService.queryBasSupplier());
     }
-    @Autowired
-    private HomeDoingBillServiceImpl hdbservice;
     @ApiOperation("查询处理中主要单据")
     @GetMapping("/queryHomeDoingBill")
     @Override
     public JsonVO<List<HomeDoingBillVO>> queryHomeDoingBill() {
-
         return JsonVO.success(hdbservice.listHomeDoingBill());
     }
 }
