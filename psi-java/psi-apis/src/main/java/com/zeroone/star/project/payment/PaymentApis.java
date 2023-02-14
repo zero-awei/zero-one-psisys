baff615ae36542971cba86c592bf4bb20529e497
Poskty <865127223@QQ.com>
2023-02-14 21:00:45 +0800

    提交领域模型 apis接口 controller中方法

--- psi-java/psi-apis/src/main/java/com/zeroone/star/project/payment/PaymentApis.java
+++ psi-java/psi-apis/src/main/java/com/zeroone/star/project/payment/PaymentApis.java
@@ -1,5 +1,8 @@
 package com.zeroone.star.project.payment;
 
+import com.zeroone.star.project.dto.payment.DeleteDTO;
+import com.zeroone.star.project.vo.JsonVO;
+
 /**
  * <p>
  * 描述：API接口定义
@@ -11,4 +14,10 @@ package com.zeroone.star.project.payment;
  * @version 1.0.0
  */
 public interface PaymentApis {
+    /**
+     * 测试API接口
+     * @param deleteDTO 包含付款单id
+     * @return 删除结果
+     */
+    JsonVO<String> deleteById(DeleteDTO deleteDTO);
 }
--- psi-java/psi-payment/src/main/java/com/zeroone/star/payment/controller/PaymentController.java
+++ psi-java/psi-payment/src/main/java/com/zeroone/star/payment/controller/PaymentController.java
@@ -1,4 +1,22 @@
 package com.zeroone.star.payment.controller;
 
-public class PaymentController {
+import com.zeroone.star.project.dto.payment.DeleteDTO;
+import com.zeroone.star.project.payment.PaymentApis;
+import com.zeroone.star.project.vo.JsonVO;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/finPayment")
+@ApiOperation(value = "预付接口")
+public class PaymentController implements PaymentApis {
+    @DeleteMapping("delete")
+    @ApiOperation(value = "删除信息")
+    @Override
+    public JsonVO<String> deleteById(DeleteDTO deleteDTO) {
+        return JsonVO.success("删除成功");
+    }
 }
