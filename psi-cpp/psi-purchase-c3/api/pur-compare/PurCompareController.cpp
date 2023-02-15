63eac5078b482e9bb9aab16e2085e5385ffa7e12
QZP <qzp@mail.ustc.edu.cn>
2023-02-15 18:01:13 +0800

    添加三个领域模型Query； 每个文件开头添加版权公告； 四个接口调试成功；

--- psi-cpp/psi-purchase-c3/api/pur-compare/PurCompareController.cpp
+++ psi-cpp/psi-purchase-c3/api/pur-compare/PurCompareController.cpp
@@ -1,3 +1,21 @@
+/*
+ Copyright Zero One Star. All rights reserved.
+
+ @Author: QZP
+ @Date: 2023/2/15 17:57:26
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+	  https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
 #include "stdafx.h"
 #include "PurCompareController.h"
 //#include "../../service/sample/SampleService.h"
@@ -17,18 +35,19 @@ JsonVO<PageVO<PurComFindBillVO>> PurCompareController::execQueryPurComFindBill(c
 	return JsonVO<PageVO<PurComFindBillVO>>(result, RS_SUCCESS);
 }
 
-JsonVO<PurComFindDetailBillVO> PurCompareController::execQueryPurComFindDetailBill(const string& query, const PayloadDTO& payload)
+JsonVO<PurComFindDetailBillVO> PurCompareController::execQueryPurComFindDetailBill(const PurComFindDetailBillQuery& query, const PayloadDTO& payload)
 {
 	////定义一个Service
 	//SampleService service;
 	////查询数据
 	//PageVO<SampleVO> result = service.listAll(query);
 	////响应结果
+
 	PurComFindDetailBillVO result;
 	return JsonVO<PurComFindDetailBillVO>(result, RS_SUCCESS);
 }
 
-JsonVO<PageVO<PurComListVO>> PurCompareController::execQueryPurComList(const string& query, const PayloadDTO& payload)
+JsonVO<PageVO<PurComListVO>> PurCompareController::execQueryPurComList(const PurComListQuery& query, const PayloadDTO& payload)
 {
 	////定义一个Service
 	//SampleService service;
@@ -39,7 +58,7 @@ JsonVO<PageVO<PurComListVO>> PurCompareController::execQueryPurComList(const str
 	return JsonVO<PageVO<PurComListVO>>(result, RS_SUCCESS);
 }
 
-JsonVO<PageVO<PurComDividedListVO>> PurCompareController::execQueryPurComDividedList(const string& query, const PayloadDTO& payload)
+JsonVO<PageVO<PurComDividedListVO>> PurCompareController::execQueryPurComDividedList(const PurComDividedListQuery& query, const PayloadDTO& payload)
 {
 	////定义一个Service
 	//SampleService service;
