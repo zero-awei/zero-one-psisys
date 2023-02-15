c5c4756744ff27ca67ad72b7210b4ee943fc0917
huajie <327445442@qq.com>
2023-02-15 11:19:35 +0800

    完成Excel表格导入相关接口定义

---
+++ psi-java/psi-payment/src/main/java/com/zeroone/star/payment/config/ComponentInit.java
@@ -0,0 +1,12 @@
+package com.zeroone.star.payment.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({
+        "com.zeroone.star.project.components.fastdfs",
+        "com.zeroone.star.project.components.easyexcel"
+})
+public class ComponentInit {
+}
