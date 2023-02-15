f64bc3db0445a54ad3d92028026ea4f7af360cd8
kun <2969842877@qq.com>
2023-02-15 23:59:09 +0800

    修改domain中不符合规范的代码

---
+++ psi-cpp/psi-purchase-c3/domain/dto/pur-compare/DelPurComDTO.h
@@ -0,0 +1,32 @@
+#ifndef _ADD_PUR_COM_DTO_
+#define _ADD_PUR_COM_DTO_
+
+
+
+#pragma once
+#ifndef _PUR_COMPARE_DTO_
+#define _PUR_COMPARE_DTO_
+
+#include "../../GlobalInclude.h"
+#include "../FileDTO.h"
+
+
+
+
+#include "../../GlobalInclude.h"
+#include "../FileDTO.h"
+class DelPurComDTO
+{
+	// 单据编号
+	CC_SYNTHESIZE(string, billId, BillId);
+
+public:
+	// 绑定JSON转换方法
+	friend void from_json(const json& j, DelPurComDTO& t) {
+		BIND_FROM_TO_NORMAL(j, t, billId);
+	}
+
+};
+
+#endif // !_ADD_PUR_COM_DTO_
+
