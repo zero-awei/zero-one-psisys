f7884c7cda8fa680ba4bdf7af1884afff080392f
weierstrass-protoconsciousness <578655045@qq.com>
2023-02-15 22:55:36 +0800

    1、完成了根据命名规范对文件夹名称、DTO名称、#ifdef名称、create...Router名称，接口名称的修改，并完成了实时响应的测试

---
+++ psi-cpp/psi-purchase-c3/api/pur-quot/PurQuotController.h
@@ -0,0 +1,47 @@
+#include "stdafx.h"
+#include "domain/dto/IDDTO.h"
+#include "../../domain/vo/JsonVO.h"
+#include "../../domain/vo/PageVO.h"
+#include "../../domain/dto/pur-quot/AddPurQuotDTO.h"
+#include "../../domain/dto/pur-quot/DelPurQuotDTO.h"
+#include "../../domain/dto/pur-quot/ModPurQuotDTO.h"
+#include "../../domain/dto/pur-quot/PurQuotModBillStatusDTO.h"
+#include "../../domain/vo/pur-quot/AddPurQuotVO.h"
+#include "../../domain/vo/pur-quot/DelPurQuotVO.h"
+#include "../../domain/vo/pur-quot/ModPurQuotVO.h"
+#include "../../domain/vo/pur-quot/PurQuotModBillStatusVO.h"
+
+#ifndef _PUR_QUOT_CONTROLLER_
+#define _PUR_QUOT_CONTROLLER_
+
+
+
+class PurQuotController
+{
+public:
+	//添加报价
+	//在添加报价中，里面还有一个查询采购询价单的选项，用于查询采购询价单
+	//明细可能还需要额外的处理，它不是一行数据，而是很多行
+	//CREATE_API_FUN_BODY_PAYLOAD(addPurQuot, execAddPurQuot, PurQuotDTO);
+	CREATE_API_FUN_BODY(addPurQuot, execAddPurQuot, AddPurQuotDTO);
+	//修改报价
+	//CREATE_API_FUN_BODY_PAYLOAD(modPurQuot, execModPurQuot, PurQuotDTO);
+	CREATE_API_FUN_BODY(modPurQuot, execModPurQuot, ModPurQuotDTO);
+	//删除报价
+	CREATE_API_FUN_BODY(delPurQuot, execDelPurQuot, DelPurQuotDTO);
+	//修改报价状态（关闭、作废、反关闭）
+	CREATE_API_FUN_BODY(purQuotModBillStatus, execPurQuotModBillStatus, PurQuotModBillStatusDTO);
+
+
+private:
+	//演示新增报价
+	JsonVO<uint64_t> execAddPurQuot(const AddPurQuotDTO& dto);
+	//演示修改报价
+	JsonVO<uint64_t> execModPurQuot(const ModPurQuotDTO& dto);
+	//演示删除报价
+	JsonVO<uint64_t> execDelPurQuot(const DelPurQuotDTO& dto);
+	//修改报价状态（关闭、作废、反关闭）
+	JsonVO<uint64_t> execPurQuotModBillStatus(const PurQuotModBillStatusDTO& dto);
+};
+
+#endif // _PUI_QUOT_CONTROLLER_
---
+++ psi-cpp/psi-purchase-c3/domain/dto/pur-quot/DelPurQuotDTO.h
@@ -0,0 +1,118 @@
+#include "stdafx.h"
+#include "../../GlobalInclude.h"
+#ifndef _DEL_PUR_QUOT_DTO_
+#define _DEL_PUR_QUOT_DTO_
+
+
+
+class DelPurQuotDTO
+{
+	//以下有一些字段，我不确定是否真的会用到，如果之后写代码，发现还需要其他字段，就添加上去；不需要再删除；
+	// 单据id
+	CC_SYNTHESIZE(long, id, Id);
+	//单据编号
+	CC_SYNTHESIZE(string, billNo, BillNo);
+	//单据日期
+	CC_SYNTHESIZE(string, billDate, BillDate);
+	//单据主题
+	CC_SYNTHESIZE(string, subject, Subject);
+	//源单号
+	CC_SYNTHESIZE(string, srcNo, SrcNo);
+	//是否临时供应商
+	CC_SYNTHESIZE(int, isTempSupplier, IsTempSupplier);
+	//供应商
+	CC_SYNTHESIZE(long, supplierId, SupplierId);
+	//临时供应商名称
+	CC_SYNTHESIZE(string, tempSupplierName, TempSupplierName);
+	//付款方式
+	CC_SYNTHESIZE(string, deliveryMethod, DeliveryMethod);
+	//交货日期
+	CC_SYNTHESIZE(string, deliveryDate, DeliveryDate);
+	//交货地点
+	CC_SYNTHESIZE(string, deliveryAddress, DeliveryAddress);
+	//联系人
+	CC_SYNTHESIZE(string, contact, Contact);
+	//联系电话
+	CC_SYNTHESIZE(string, phone, Phone);
+	//传真
+	CC_SYNTHESIZE(string, tax, Tax);
+	//电子邮件
+	CC_SYNTHESIZE(string, email, Email);
+	//数量
+	CC_SYNTHESIZE(double, qty, Qty);
+	//金额
+	CC_SYNTHESIZE(double, amt, Amt);
+	//附件
+	CC_SYNTHESIZE(string, attachment, Attachment);
+	//备注
+	CC_SYNTHESIZE(string, remark, Remark);
+	//单据阶段
+	CC_SYNTHESIZE(int, billStage, BillStage);
+	//审核人
+	CC_SYNTHESIZE(string, approver, Approver);
+	//核批结果类型
+	CC_SYNTHESIZE(int, approvalResultType, ApprovalResultType);
+	//核批意见
+	CC_SYNTHESIZE(string, approvalRemark, ApprovalRemark);
+	//是否生效
+	CC_SYNTHESIZE(int, isEffective, IsEffective);
+	//生效时间
+	CC_SYNTHESIZE(string, effectiveTime, EffectiveTime);
+	//是否已关闭
+	CC_SYNTHESIZE(int, isClosed, IsClosed);
+	//是否作废
+	CC_SYNTHESIZE(int, isVoided, IsVoided);
+	//创建部门
+	CC_SYNTHESIZE(string, sysOrdCode, SysOrdCode);
+	//创建人
+	CC_SYNTHESIZE(string, createBy, CreateBy);
+	//创建时间
+	CC_SYNTHESIZE(string, createTime, CreateTime);
+	//修改人
+	CC_SYNTHESIZE(string, updateBy, UpdateBy);
+	//修改时间
+	CC_SYNTHESIZE(string, updateTime, updateTime);
+
+public:
+	//绑定JSON转换方法
+	friend void from_json(const json& j, DelPurQuotDTO& t) {
+		BIND_FROM_TO_L(j, t, id);
+		BIND_FROM_TO_L(j, t, supplierId);
+
+		BIND_FROM_TO_I(j, t, isTempSupplier);
+		BIND_FROM_TO_I(j, t, isEffective);
+		BIND_FROM_TO_I(j, t, isClosed);
+		BIND_FROM_TO_I(j, t, isVoided);
+		BIND_FROM_TO_I(j, t, approvalResultType);
+		BIND_FROM_TO_I(j, t, billStage);
+
+		BIND_FROM_TO_D(j, t, qty);
+		BIND_FROM_TO_D(j, t, amt);
+
+		BIND_FROM_TO_NORMAL(j, t, billNo);
+		BIND_FROM_TO_NORMAL(j, t, billDate);
+		BIND_FROM_TO_NORMAL(j, t, subject);
+		BIND_FROM_TO_NORMAL(j, t, srcNo);
+		BIND_FROM_TO_NORMAL(j, t, tempSupplierName);
+		BIND_FROM_TO_NORMAL(j, t, deliveryMethod);
+		BIND_FROM_TO_NORMAL(j, t, deliveryDate);
+		BIND_FROM_TO_NORMAL(j, t, deliveryAddress);
+		BIND_FROM_TO_NORMAL(j, t, contact);
+		BIND_FROM_TO_NORMAL(j, t, phone);
+		BIND_FROM_TO_NORMAL(j, t, tax);
+		BIND_FROM_TO_NORMAL(j, t, email);
+		BIND_FROM_TO_NORMAL(j, t, attachment);
+		BIND_FROM_TO_NORMAL(j, t, remark);
+		BIND_FROM_TO_NORMAL(j, t, approver);
+
+		BIND_FROM_TO_NORMAL(j, t, approvalRemark);
+		BIND_FROM_TO_NORMAL(j, t, effectiveTime);
+		BIND_FROM_TO_NORMAL(j, t, sysOrdCode);
+		BIND_FROM_TO_NORMAL(j, t, createBy);
+		BIND_FROM_TO_NORMAL(j, t, createTime);
+		BIND_FROM_TO_NORMAL(j, t, updateBy);
+		BIND_FROM_TO_NORMAL(j, t, updateTime);
+	}
+};
+
+#endif // !_DEL_PUR_QUOT_DTO_
