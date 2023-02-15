95e7de3d6d3e7972f9b4a47b713fb0d06743ff8b
“pyramidegg” <1731362930@qq.com>
2023-02-15 21:19:57 +0800

    生成了fin_payment和fin_payment_entry表

---
+++ psi-java/psi-paymentmanagement/src/main/java/com/zeroone/star/paymentmanagement/service/IFinPaymentEntryService.java
@@ -0,0 +1,16 @@
+package com.zeroone.star.paymentmanagement.service;
+
+import com.zeroone.star.paymentmanagement.entity.FinPaymentEntry;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 付款单明细 服务类
+ * </p>
+ *
+ * @author pyramid
+ * @since 2023-02-15
+ */
+public interface IFinPaymentEntryService extends IService<FinPaymentEntry> {
+
+}
---
+++ psi-java/psi-paymentmanagement/src/main/java/com/zeroone/star/paymentmanagement/service/impl/FinPaymentEntryServiceImpl.java
@@ -0,0 +1,20 @@
+package com.zeroone.star.paymentmanagement.service.impl;
+
+import com.zeroone.star.paymentmanagement.entity.FinPaymentEntry;
+import com.zeroone.star.paymentmanagement.mapper.FinPaymentEntryMapper;
+import com.zeroone.star.paymentmanagement.service.IFinPaymentEntryService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 付款单明细 服务实现类
+ * </p>
+ *
+ * @author pyramid
+ * @since 2023-02-15
+ */
+@Service
+public class FinPaymentEntryServiceImpl extends ServiceImpl<FinPaymentEntryMapper, FinPaymentEntry> implements IFinPaymentEntryService {
+
+}
