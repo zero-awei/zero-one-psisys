49f83efbbd5982d09cd1f0e81910748bedfbd19a
juloo <2399542479@qq.com>
2023-02-15 16:55:34 +0800

    采购预付申请domain

--- psi-cpp/arch-demo/main.cpp
+++ psi-cpp/arch-demo/main.cpp
@@ -43,7 +43,7 @@ bool getStartArg(int argc, char* argv[]) {
 	// 数据库连接信息
 	std::string dbUsername = "root";
 	std::string dbPassword = "123456";
-	std::string dbName = "test";
+	std::string dbName = "text";
 	std::string dbHost = "192.168.220.128";
 	int dbPort = 3306;
 	int dbMax = 25;
