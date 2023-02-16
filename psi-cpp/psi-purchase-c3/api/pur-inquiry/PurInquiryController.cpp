5fbc06606e163cbfdccfcd87348a9c33a0b874a5
prectice <1058481077@qq.com>
2023-02-16 19:20:05 +0800

    提交修改

--- psi-cpp/psi-purchase-c3/api/pur-inquiry/PurInquiryController.cpp
+++ psi-cpp/psi-purchase-c3/api/pur-inquiry/PurInquiryController.cpp
@@ -1,26 +1,32 @@
 #include "stdafx.h"
 #include "PurInquiryController.h"
 
+//添加采购询价
 JsonVO<AddPurInquiryVO> PurInquiryController::execAddPurInquiry(const AddPurInquiryDTO& dto)
 {
 	JsonVO<AddPurInquiryVO> result;
+	AddPurInquiryVO vo;
 
 
-
-
+	result.success(vo);
 	return result;
 }
 
+//修改采购询价
 JsonVO<AddPurInquiryVO> PurInquiryController::execModifyPurInquiry(const AddPurInquiryDTO& dto)
 {
 	JsonVO<AddPurInquiryVO> result;
+	AddPurInquiryVO vo;
 
 
+	
 
 
+	result.success(vo);
 	return result;
 }
 
+//删除采购询价
 JsonVO<uint64_t> PurInquiryController::execRemovePurInquiry(const PurInquiryRemoveDTO& dto)
 {
 	JsonVO<uint64_t> result;
@@ -31,7 +37,7 @@ JsonVO<uint64_t> PurInquiryController::execRemovePurInquiry(const PurInquiryRemo
 	return result;
 }
 
-
+//修改单据编号
 JsonVO<uint64_t> PurInquiryController::execModifyPurQuotModBillStatus(const PurInquiryModBillStatusDTO& dto)
 {
 	JsonVO<uint64_t> result;
@@ -42,26 +48,27 @@ JsonVO<uint64_t> PurInquiryController::execModifyPurQuotModBillStatus(const PurI
 	return result;
 }
 
+//文件导入
 JsonVO<PurInquiryIntoVO> PurInquiryController::execPurInquiryInto(const PurInquiryIntoDTO& dto)
 {
 	JsonVO<PurInquiryIntoVO> result;
+	PurInquiryIntoVO vo;
 
 
 
-
-
+	result.success(vo);
 	return result;
 }
 
-
+//文件导出
 JsonVO<std::string> PurInquiryController::execPurInquiryExport(const PurInquiryExportQuery& query)
 {
 
 	JsonVO<std::string> result;
+	string fileName = "d//f//a.txt";
 
 
-
-
+	result.success(fileName);
 	return result;
 }
 
