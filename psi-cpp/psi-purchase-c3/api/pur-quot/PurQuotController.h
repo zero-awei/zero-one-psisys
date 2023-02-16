2f048e393e82749a2110b052c7c018c78b2c5e74
weierstrass-protoconsciousness <578655045@qq.com>
2023-02-16 17:44:39 +0800

    1、完成测通API，并对DTO多余字段进行了删减； 2、待完成：APIPost上填写对应字段；

--- psi-cpp/psi-purchase-c3/api/pur-quot/PurQuotController.h
+++ psi-cpp/psi-purchase-c3/api/pur-quot/PurQuotController.h
@@ -20,8 +20,6 @@ class PurQuotController
 {
 public:
 	//添加报价
-	//在添加报价中，里面还有一个查询采购询价单的选项，用于查询采购询价单
-	//明细可能还需要额外的处理，它不是一行数据，而是很多行
 	//CREATE_API_FUN_BODY_PAYLOAD(addPurQuot, execAddPurQuot, PurQuotDTO);
 	CREATE_API_FUN_BODY(addPurQuot, execAddPurQuot, AddPurQuotDTO);
 	//修改报价
