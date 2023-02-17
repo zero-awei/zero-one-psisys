df7ec8a4d533db0ad7828ccc53ece1e91a925a7b
李嘉鑫 <1509230273@qq.com>
2023-02-17 12:18:57 +0800

    guo

---
+++ psi-java/psi-doc/src/main/java/com/zeroone/star/doc/DocApplication.java
@@ -0,0 +1,12 @@
+package com.zeroone.star.doc;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DocApplication.class,args);
+    }
+}
