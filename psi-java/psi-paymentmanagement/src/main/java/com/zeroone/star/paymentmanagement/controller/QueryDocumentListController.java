d24b4ee752ff06287a3562a00e3695777ed95b67
achuan <765026654@qq.com>
2023-02-17 16:08:47 +0800

    定义好service controller  准备测试

--- psi-java/psi-paymentmanagement/src/main/java/com/zeroone/star/paymentmanagement/controller/QueryDocumentListController.java
+++ psi-java/psi-paymentmanagement/src/main/java/com/zeroone/star/paymentmanagement/controller/QueryDocumentListController.java
@@ -7,24 +7,30 @@ import com.zeroone.star.project.query.paymentmanagement.QueryDocumentListQuery;
 import com.zeroone.star.project.vo.JsonVO;
 import com.zeroone.star.project.vo.PageVO;
 import com.zeroone.star.project.vo.paymentmanagement.QueryDocumentListVO;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/paymentmanagement/querydocumentlist")
+@RequestMapping("/paymentmanagement/")
+@Api(tags = "付款管理-采购付款")
 @ApiModel("单据表单查询接口")
 public class QueryDocumentListController implements PaymentManagementApis {
     private IQueryDocumentListService service;
-
-    @GetMapping("/queryDocumentList")
+    @PostMapping("/queryDocumentList")
+    @ApiOperation(value="查询单据列表数据")
     @Override
     //根据查询的条件查询单据列表结果，传入条件为：单据编号，单据日期，单据主题，供应商，单据阶段，已生效，已关闭，已作废。
     public JsonVO<PageVO<QueryDocumentListVO>> queryDocumentList(QueryDocumentListQuery condition) {
 
-        return JsonVO.success(service.queryDocumentList(condition));
+//        return JsonVO.success(service.queryDocumentList(condition));
+        return null;
     }
+
 
 
 }
