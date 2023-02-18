3532d7decc02fed6eb075fbb42b50c98c0f2ffac
kun <2969842877@qq.com>
2023-02-18 14:35:15 +0800

    补充pur-out查询指定单据详细信息的query和vo

--- psi-cpp/psi-purchase-c3/domain/query/pur-quot/PurQuotFindDetailBillQuery.h
+++ psi-cpp/psi-purchase-c3/domain/query/pur-quot/PurQuotFindDetailBillQuery.h
@@ -6,12 +6,35 @@
 
 class PurQuotFindDetailBillQuery : public PageQuery
 {
-
 	// 单据编号
-	CC_SYNTHESIZE(string, receiptId, ReceiptId);
+	CC_SYNTHESIZE(string, bill_no, Bill_no);
+	//-----------------------不需要下面的内容(阿坤)
+	////单据日期--开始, 数据库里面没有这个字段
+	//CC_SYNTHESIZE(string, bill_date_begin, Bill_date_begin);
+	////单据日期--结束
+	//CC_SYNTHESIZE(string, bill_date_end, Bill_date_end);
+	//// 单据主题
+	//CC_SYNTHESIZE(string, subject, Subject);
+	//// 单据阶段
+	//CC_SYNTHESIZE(string, bill_stage, Bill_stage);
+	//// 已生效
+	//CC_SYNTHESIZE(string, is_effective, Is_effective);
+	//// 已关闭
+	//CC_SYNTHESIZE(string, is_closed, Is_closed);
+	//// 已作废
+	//CC_SYNTHESIZE(string, is_voided, Is_voided);
 public:
 	friend void from_json(const json& j, PurQuotFindDetailBillQuery& t) {
-		BIND_FROM_TO_NORMAL(j, t, receiptId);
+		BIND_FROM_TO_I(j, t, pageIndex);
+		BIND_FROM_TO_I(j, t, pageSize);
+		BIND_FROM_TO_NORMAL(j, t, bill_no);
+		//BIND_FROM_TO_NORMAL(j, t, bill_date_begin);
+		//BIND_FROM_TO_NORMAL(j, t, bill_date_end);
+		//BIND_FROM_TO_NORMAL(j, t, subject);
+		//BIND_FROM_TO_NORMAL(j, t, bill_stage);
+		//BIND_FROM_TO_NORMAL(j, t, is_effective);
+		//BIND_FROM_TO_NORMAL(j, t, is_closed);
+		//BIND_FROM_TO_NORMAL(j, t, is_voided);
 	}
 };
 #endif
--- psi-cpp/psi-purchase-c3/domain/vo/pur-quot/PurQuotFindDetailBillVO.h
+++ psi-cpp/psi-purchase-c3/domain/vo/pur-quot/PurQuotFindDetailBillVO.h
@@ -3,16 +3,90 @@
 #define _PUR_QUOT_FIND_DETAIL_BILL_VO_H_
 #include "../../GlobalInclude.h"
 
+//多表查询 数据库表pur-quot和pur-quot-entry
+//pur-quot的字段不带前缀
+//pur-quot-entry的字段带entry前缀
 class PurQuotFindDetailBillVO
 {
-	// 单据编号
-	CC_SYNTHESIZE(string, billNo, BillNo);
-	// 单据日期
-	CC_SYNTHESIZE(string, billDate, BillDate);
-	// 单据阶段
-	CC_SYNTHESIZE(string, billStage, BillStage);
+//pur-quot的字段
+	//单据编号
+	CC_SYNTHESIZE(string, bill_no, Bill_no);
+	//单据日期
+	CC_SYNTHESIZE(string, bill_date, Bill_date);
+	//单据阶段
+	CC_SYNTHESIZE(string, bill_stage, Bill_stage);
+	//已生效
+	CC_SYNTHESIZE(int, is_effective, Is_effective);
+	//已关闭
+	CC_SYNTHESIZE(int, is_closed, Is_closed);
+	//已作废
+	CC_SYNTHESIZE(int, is_voided, Is_voided);
+	//单据主题
+	CC_SYNTHESIZE(string, subject, Subject);
+	//采购询价单
+	//是否临时供应商
+	CC_SYNTHESIZE(int, is_temp_supplier, Is_temp_supplier);
+	//供应商
+	CC_SYNTHESIZE(string, supplier_id, Supplier_id);
+	//临时供应商名称
+	CC_SYNTHESIZE(string, supplier_name, Supplier_name);
+	//付款方式
+	CC_SYNTHESIZE(string, payment_method, Payment_method);
+	//交货日期
+	CC_SYNTHESIZE(string, delivery_time, Delivery_time);
+	//交货地点
+	CC_SYNTHESIZE(string, delivery_place, Delivery_place);
+	//联系人
+	CC_SYNTHESIZE(string, contact, Contact);
+	//联系电话
+	CC_SYNTHESIZE(string, phone, Phone);
+	//传真
+	CC_SYNTHESIZE(string, fax, Fax);
+	//电子邮件
+	CC_SYNTHESIZE(string, email, Email);
+	//备注
+	CC_SYNTHESIZE(string, remark, Remark);
+	//核批意见
+	CC_SYNTHESIZE(string, approval_remark, Approval_remark);
+	//附件
+	CC_SYNTHESIZE(string, attachment, Attachment);
+	//核批结果
+	CC_SYNTHESIZE(string, approval_result_type, Approval_result_type);
+
+//----明细(pur-quot-entry的字段)
+	//源单分录号
+	CC_SYNTHESIZE(string, entry_src_no, Entry_src_no);
+	//物料
+	CC_SYNTHESIZE(string, entry_material_id, Entry_material_id);
+	//规格型号
+	//单位
+	CC_SYNTHESIZE(string, entry_unit_id, Entry_unit_id);
+	//数量
+	CC_SYNTHESIZE(string, entry_qty, Entry_qty);
+	//税率
+	CC_SYNTHESIZE(string, entry_tax_rate, Entry_tax_rate);
+	//含税单价
+	CC_SYNTHESIZE(string, entry_price, Entry_price);
+	//折扣率
+	CC_SYNTHESIZE(string, entry_discount_rate, Entry_discount_rate);
+	//含税金额
+	CC_SYNTHESIZE(string, entry_amt, Entry_amt);
+	//备注
+	CC_SYNTHESIZE(string, entry_remark, Entry_remark);
+	//自定义1
+	CC_SYNTHESIZE(string, entry_custom1, Entry_custom1);
+	//自定义2
+	CC_SYNTHESIZE(string, entry_custom2, Entry_custom2);
 public:
 	// 绑定JSON转换方法
-	BIND_TO_JSON(PurQuotFindDetailBillVO, billNo, billDate, billStage);
+	BIND_TO_JSON(PurQuotFindDetailBillVO,
+//pur-quot字段
+ bill_no, bill_date, bill_stage, is_effective, is_closed, is_voided,
+		subject, is_temp_supplier, supplier_id, supplier_name, payment_method, delivery_time, delivery_place,
+		contact, phone, fax, email, remark, approval_remark, attachment, approval_result_type,
+//pur-quot-entry字段
+entry_src_no, entry_material_id, entry_unit_id, entry_qty, entry_tax_rate, entry_price, entry_discount_rate,
+entry_amt, entry_remark, entry_custom1, entry_custom2);
 };
 #endif // !_PUR_QUOT_FIND_DETAIL_BILL_VO_H_
+
