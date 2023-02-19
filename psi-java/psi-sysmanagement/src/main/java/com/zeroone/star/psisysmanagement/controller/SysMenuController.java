32e8802eeed95265bdcb383d85319ecd2719888b
Xiaoyolee <Xiaoyolee@126.com>
2023-02-19 22:04:29 +0800

    编写Controller

--- psi-java/psi-sysmanagement/src/main/java/com/zeroone/star/psisysmanagement/controller/SysMenuController.java
+++ psi-java/psi-sysmanagement/src/main/java/com/zeroone/star/psisysmanagement/controller/SysMenuController.java
@@ -1,22 +1,26 @@
 package com.zeroone.star.psisysmanagement.controller;
 
 import com.zeroone.star.project.dto.sysmanagement.menumanagement.MenuDTO;
+import com.zeroone.star.project.query.sysmanagement.menumanagement.SingleMenuQuery;
 import com.zeroone.star.project.vo.JsonVO;
 import com.zeroone.star.project.vo.ResultStatus;
 import com.zeroone.star.psisysmanagement.entity.SysMenu;
+import com.zeroone.star.psisysmanagement.service.ISysMenuService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
+import java.awt.*;
 import java.util.List;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
- * @author 归、深夜梦想家 
+ * @author 归、深夜梦想家
  * @since 2023-02-14
  */
 
@@ -25,34 +29,55 @@ import java.util.List;
 @RequestMapping("/sysmanagement/menumanagement/sys-menu")
 public class SysMenuController {
 
+    @Resource
+    private ISysMenuService iSysMenuService;
+    @Resource
+    private SingleMenuQuery singleMenuQuery;
+
     @ResponseBody
     @SneakyThrows
     @ApiOperation(value = "新增菜单")
     @PostMapping("/add")
-    public JsonVO<String> addMenu(SysMenu sysMenu){
-        return JsonVO.create("新增成功", ResultStatus.SUCCESS);
+    public JsonVO<String> addMenu(MenuDTO menuDTO) {
+
+        JsonVO<ResultStatus> resultStatus = iSysMenuService.addMenu(menuDTO);
+
+        return resultStatus.getData().getCode() == 9999
+                ? JsonVO.create("增加失败", ResultStatus.FAIL)
+                : JsonVO.create("新增成功", ResultStatus.SUCCESS);
     }
 
     @SneakyThrows
     @ApiOperation(value = "修改菜单")
     @PutMapping("/update")
-    public JsonVO<String> updateMenu(int id){
-        return JsonVO.create("修改成功", ResultStatus.SUCCESS);
+    public JsonVO<String> updateMenu(MenuDTO menuDTO) {
+
+        JsonVO<ResultStatus> resultStatus = iSysMenuService.updateMenu(menuDTO);
+
+        return resultStatus.getData().getCode() == 9999
+                ? JsonVO.create("修改失败", ResultStatus.FAIL)
+                : JsonVO.create("修改成功", ResultStatus.SUCCESS);
     }
 
     @ResponseBody
     @SneakyThrows
     @ApiOperation(value = "删除菜单")
     @DeleteMapping("/delete")
-    public JsonVO<String> deleteMenu(int id){
-        return JsonVO.create("删除成功", ResultStatus.SUCCESS);
+    public JsonVO<String> deleteMenu(int id) {
+
+        singleMenuQuery.setId(String.valueOf(id));
+        JsonVO<ResultStatus> resultStatus = iSysMenuService.deleteMenu(singleMenuQuery);
+
+        return resultStatus.getData().getCode() == 9999
+                ? JsonVO.create("删除失败", ResultStatus.FAIL)
+                : JsonVO.create("删除成功", ResultStatus.SUCCESS);
     }
 
 
     @SneakyThrows
     @ApiOperation(value = "查询菜单")
     @GetMapping("/query")
-    public JsonVO<List<MenuDTO>> queryMenu(int RId){
+    public JsonVO<List<MenuDTO>> queryMenu(int RId) {
         return new JsonVO<List<MenuDTO>>();
     }
 
