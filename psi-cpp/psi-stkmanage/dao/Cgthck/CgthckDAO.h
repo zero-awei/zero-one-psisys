9973b931fbacd06f82c95421d8c2303be3027c8f
zer00000001 <1571454955@qq.com>
2023-02-20 21:42:03 +0800

    新增采购退货出库DAO

---
+++ psi-cpp/psi-stkmanage/dao/Cgthck/CgthckDAO.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef _CGTHCK_DAO_
+#define _CGTHCK_DAO_
+#include "BaseDAO.h"
+#include "../../domain/do/Cgthck/CgthckDO.h"
+class CgthckDAO : public BaseDAO
+{
+};
+
+
+
+#endif // !_CGTHCK_DAO_
--- psi-cpp/psi-stkmanage/service/Cgthck/CgthckService.cpp
+++ psi-cpp/psi-stkmanage/service/Cgthck/CgthckService.cpp
@@ -1,35 +1,16 @@
 #include "stdafx.h"
 #include "CgthckService.h"
 
-//PageVO<QueryCgthckBillDetailVO> CgthckService::listAll(const QueryCgthckBillQuery& query)
-//{
-//    //// 构建返回对象
-//    //PageVO<QueryCgthckBillDetailVO> pages;
-//    //pages.setPageIndex(query.getPageIndex());
-//    //pages.setPageSize(query.getPageSize());
-//
-//    //// 查询数据总条数
-//    //CgthckBillDetailDO obj;
-//    //obj.setBillNo(query.getBillNo());
-//    //obj.setBillDate(query.getBillDate());
-//
-//    //// 分页查询数据
-//    //pages.setTotal(0);
-//    //pages.calcPages();
-//    //list<CgthckBillDetailDO> result = 
-//}
-//
-//uint64_t CgthckService::saveData(const AddCgthckBillDTO& dto)
-//{
-//    //// 组装数据
-//    //CgthckBillDetailDO data;
-//    //data.setBillNo(dto.getBillNo());
-//    //data.setBillDate(dto.getBillDate());
-//    //data.setSupplierId(dto.getSupplierId());
-//    //data.setSrcNo(dto.getSrcNo());
-//    //// 执行数据添加
-//
-//}
+
+PageVO<QueryCgthckBillDetailVO> CgthckService::listAll(const QueryCgthckBillQuery& query)
+{
+    return PageVO<QueryCgthckBillDetailVO>();
+}
+
+uint64_t CgthckService::saveData(const AddCgthckBillDTO& dto)
+{
+    return uint64_t();
+}
 
 bool CgthckService::updateData(const ModifyCgthckBillDTO& dto)
 {
