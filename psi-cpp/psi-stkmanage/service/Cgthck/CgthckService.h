9c4e66abc26e23a1ba727d4fedac27af9f6af58f
zer00000001 <1571454955@qq.com>
2023-02-20 21:31:13 +0800

    在合并基础上新建新增分支

---
+++ psi-cpp/psi-stkmanage/service/Cgthck/CgthckService.h
@@ -0,0 +1,34 @@
+#pragma once
+#ifndef _CGTHCK_SERVICE_
+#define _CGTHCK_SERVICE_
+
+#include "../../domain/vo/PageVO.h"
+#include "../../domain/do/Cgthck/CgthckDO.h"
+#include "../../domain/vo/Cgthck/QueryCgthckBillVO.h"
+#include "../../domain/query/Cgthck/QueryCgthckBillQuery.h"
+#include "../../domain/dto/Cgthck/AddCgthckBillDTO.h"
+#include "../../domain/dto/Cgthck/DeleteCgthckBillDTO.h"
+#include "../../domain/dto/Cgthck/ExportCgthckFileDTO.h"
+#include "../../domain/dto/Cgthck/ImportCgthckFileDTO.h"
+#include "../../domain/dto/Cgthck/ModifyCgthckBillDTO.h"
+
+
+class CgthckService
+{
+	// 分页查询数据
+	PageVO<QueryCgthckBillDetailVO> listAll(const QueryCgthckBillQuery& query);
+	// 添加采购退货出库单(保存/提交)
+	uint64_t saveData(const AddCgthckBillDTO& dto);
+	// 修改采购退货出库单（保存/提交/审核）
+	bool updateData(const ModifyCgthckBillDTO& dto);
+	// 删除采购退货出库单
+	bool removeData(uint64_t id);
+	// 修改单据状态（关闭/作废/反关闭）
+	// 导入
+	// 导出
+};
+
+
+#endif // !_CGTHCK_SERVICE_
+
+
