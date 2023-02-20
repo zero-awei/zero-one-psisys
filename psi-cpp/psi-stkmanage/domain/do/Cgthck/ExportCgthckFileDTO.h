a3a6b55e3a3fb1a2f107cf78a127eaa0f9b54266
zer00000001 <1571454955@qq.com>
2023-02-20 21:32:19 +0800

    实现采购退货出库导入导出DTO

---
+++ psi-cpp/psi-stkmanage/domain/do/Cgthck/ExportCgthckFileDTO.h
@@ -0,0 +1,20 @@
+#ifndef _EXPORT_CGTHCK_FILE_DTO_
+#define _EXPORT_CGTHCK_FILE_DTO_
+#include "../../GlobalInclude.h"
+
+/**
+* * 采购退货出库(红入), 导出采购退货出库单数据传输对象
+*/
+class ExportCgthckFileDTO
+{
+	CC_SYNTHESIZE(std::list<std::string>, ids, Ids);
+public:
+	// 绑定Json转换方法
+	friend void from_json(const json& j, ExportCgthckFileDTO& t)
+	{
+		BIND_FROM_TO_OBJ(j, t, ids, std::list<std::string>);
+	}
+};
+
+#endif // !_EXPORT_CGTHCK_FILE_DTO_
+
