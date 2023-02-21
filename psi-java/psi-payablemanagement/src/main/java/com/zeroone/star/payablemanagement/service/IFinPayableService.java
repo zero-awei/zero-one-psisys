16d49daba489e34b1f9c36559e859fec004db6d3
kumiko42 <58432748+kumiko42@users.noreply.github.com>
2023-02-22 00:45:47 +0800

    修改小bug并测试成功

--- psi-java/psi-payablemanagement/src/main/java/com/zeroone/star/payablemanagement/service/IFinPayableService.java
+++ psi-java/psi-payablemanagement/src/main/java/com/zeroone/star/payablemanagement/service/IFinPayableService.java
@@ -2,6 +2,11 @@ package com.zeroone.star.payablemanagement.service;
 
 import com.zeroone.star.payablemanagement.entity.FinPayable;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.zeroone.star.project.query.payablemanagement.PayableBillNoQuery;
+import com.zeroone.star.project.query.payablemanagement.PayableQuery;
+import com.zeroone.star.project.vo.PageVO;
+import com.zeroone.star.project.vo.payablemanagement.PayableVO;
+import org.springframework.validation.annotation.Validated;
 
 /**
  * <p>
@@ -12,5 +17,21 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2023-02-14
  */
 public interface IFinPayableService extends IService<FinPayable> {
+    public PageVO<PayableVO> getAll(PayableQuery query);
+
+    public PayableVO getById(@Validated PayableBillNoQuery query);
+    /**
+     * 根据条件分页查询
+     * @param query 查询条件
+     * @return 查询结果
+     */
+    PageVO<PayableVO> queryAll(PayableQuery query);
+
+    /**
+     * 根据单据编号查询
+     * @param billNo 单据编号
+     * @return 查询结果
+     */
+    PayableVO getByBillNo(String billNo);
 
 }
