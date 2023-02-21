c3edba06a8198e5a8995fd34c598b8ab494bc1f2
zer00000001 <1571454955@qq.com>
2023-02-21 10:20:39 +0800

    初步构建dao结构

--- psi-cpp/psi-stkmanage/dao/Cgthck/CgthckDAO.cpp
+++ psi-cpp/psi-stkmanage/dao/Cgthck/CgthckDAO.cpp
@@ -1,2 +1,22 @@
 #include "stdafx.h"
 #include "CgthckDAO.h"
+
+uint64_t CgthckDAO::count(const CgthckDO& iobj)
+{
+	return uint64_t();
+}
+
+uint64_t CgthckDAO::insert(const CgthckDO& iobj)
+{
+	return uint64_t();
+}
+
+int CgthckDAO::update(const CgthckDO& iobj)
+{
+	return 0;
+}
+
+int CgthckDAO::deleteById(const CgthckDO& iobj)
+{
+	return 0;
+}
--- psi-cpp/psi-stkmanage/dao/Cgthck/CgthckDAO.h
+++ psi-cpp/psi-stkmanage/dao/Cgthck/CgthckDAO.h
@@ -3,8 +3,20 @@
 #define _CGTHCK_DAO_
 #include "BaseDAO.h"
 #include "../../domain/do/Cgthck/CgthckDO.h"
+
+using CgthckDO = CgthckBillDetailDO;
+ 
 class CgthckDAO : public BaseDAO
 {
+public:
+	// 统计数据条数
+	uint64_t count(const CgthckDO& iobj);
+	// 插入数据
+	uint64_t insert(const CgthckDO& iobj);
+	// 修改数据
+	int update(const CgthckDO& iobj);
+	// 删除通过单号数据
+	int deleteById(const CgthckDO& iobj);
 };
 
 
