019306f94b2fa79e2b8f018ffc4d3033c622f748
zxh <1019861458@qq.com>
2023-02-21 10:06:30 +0800

    添加sys_user_role/depart表

---
+++ psi-cpp/psi-stkmanage/domain/do/Cgthck/SysUserDepartDO.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef _S_U_D_DO
+#define _S_U_D_DO
+
+#include "../../GlobalInclude.h"
+
+
+class SysUserDepartDO {
+	// ±àºÅ
+	CC_SYNTHESIZE(uint64_t, id, Id);
+
+	CC_SYNTHESIZE(string, userId, UserId);
+
+	CC_SYNTHESIZE(string, depId, DepId);
+	
+};
+
+
+
+
+
+
+
+#endif
