4133c78eff50194344d3596c2902883f70485fd0
kumiko42 <58432748+kumiko42@users.noreply.github.com>
2023-02-21 13:09:26 +0800

    修改目录结构

--- psi-java/psi-payablemanagement/src/main/java/com/zeroone/star/payablemanagement/PayableManagementApplication.java
+++ psi-java/psi-payablemanagement/src/main/java/com/zeroone/star/payablemanagement/PayableManagementApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class AccountPayableApplication {
+public class PayableManagementApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(com.zeroone.star.payablemanagement.AccountPayableApplication.class, args);
+        SpringApplication.run(PayableManagementApplication.class, args);
     }
 
 }
