9c1f6db557b7f23c668afd82df947eae979b54d6
kun <2969842877@qq.com>
2023-02-23 20:57:01 +0800

    修改Dao.cpp

--- psi-cpp/psi-purchase-c3/dao/pur-quot/PurQuotDAO.cpp
+++ psi-cpp/psi-purchase-c3/dao/pur-quot/PurQuotDAO.cpp
@@ -97,20 +97,20 @@ list<PurQuotEntryDO> PurQuotDAO::selectPurQuotDividedList(const PurQuotEntryDO& 
 }
 
 
-//查询指定单据的详细信息
+//查询指定单据的详细信息---pur_quot表
 list<PurQuotDO> PurQuotDAO::selectPurQuotBase(const PurQuotDO& obj) {
 	stringstream sql;
 	sql << "SELECT bill_no, bill_date, bill_stage, is_effective, is_closed, is_voided, subject,\
 		is_temp_supplied, supplier_id, payment_method, delivery_time, delivery_place, \
 	contact, phone, fax, email, remark, approval_mark, attachment, approval_result_type ,\
 	 effective_time,approver, bpmi_instance_id, bpmi_instance_id, sys_org_code, create_by, update_time, \
-	update_by, is_auto, is_rubric, src_bill_type FROM pur_quot_entry";
+	update_by, is_auto, is_rubric, src_bill_type FROM pur_quot";
 	PUR_QUOT_ENTRY_TERAM_PARSE(obj, sql);
 	PurQuotBaseMapper mapper;
 	string sqlStr = sql.str();
 	return sqlSession->executeQuery<PurQuotDO, PurQuotBaseMapper>(sqlStr, mapper, params);
 }
-//查询指定单据的详细信息
+//查询指定单据的详细信息----pur_quot_entry表
 list<PurQuotEntryDO> PurQuotDAO::selectPurQuotDetail(const PurQuotEntryDO& obj) {
 	stringstream sql;
 	sql << "SELECT src_no, material_id, unit_id, qty, tax_rate, price, discount_rate,\
