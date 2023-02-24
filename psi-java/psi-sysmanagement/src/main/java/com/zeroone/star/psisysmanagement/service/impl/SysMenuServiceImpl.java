18bd9b2e869ef14c8939e431b3c86f9c0aa4c4dc
Wang Yuxing <1059637081@qq.com>
2023-02-24 21:02:59 +0800

    修改了sysmanagement-menumanagement模块部分未合并成功的代码

--- psi-java/psi-sysmanagement/src/main/java/com/zeroone/star/psisysmanagement/service/impl/SysMenuServiceImpl.java
+++ psi-java/psi-sysmanagement/src/main/java/com/zeroone/star/psisysmanagement/service/impl/SysMenuServiceImpl.java
@@ -45,17 +45,30 @@ public class SysMenuServiceImpl extends ServiceImpl<SysMenuMapper, SysMenu> impl
     @Override
     public JsonVO<List<MenuVO>> queryMenus(SysMenuQuery sysMenuQuery) {
 
-        QueryWrapper<SysMenu> menuDTOQueryWrapper = new QueryWrapper<>();
-        menuDTOQueryWrapper.eq("parent_id", sysMenuQuery.getParentId());
-
-        List<SysMenu> sysMenus = baseMapper.selectList(menuDTOQueryWrapper);
         ArrayList<MenuVO> menuVOS = new ArrayList<>();
-        for (SysMenu sysMenu : sysMenus) {
+        String id = sysMenuQuery.getId();
+        String parentId = sysMenuQuery.getParentId();
+
+        //传入的参数有id时，返回该id对应的菜单
+        if (id != null) {
+            SysMenu sysMenu = baseMapper.selectById(id);
             MenuVO menuVO = BeanUtil.copyProperties(sysMenu, MenuVO.class);
             menuVOS.add(menuVO);
         }
 
-        return JsonVO.create(menuVOS, ResultStatus.SUCCESS);
+        //传入的参数有parentId时，返回该parentId对应的子id(次级子节点，非全部子节点)
+        if (parentId != null) {
+            QueryWrapper<SysMenu> queryWrapper = new QueryWrapper<>();
+            queryWrapper.eq("parent_id", sysMenuQuery.getParentId());
+            List<SysMenu> sysMenus = baseMapper.selectList(queryWrapper);
+            for (SysMenu sysMenu : sysMenus) {
+                MenuVO menuVO = BeanUtil.copyProperties(sysMenu, MenuVO.class);
+                menuVOS.add(menuVO);
+            }
+        }
+
+        return menuVOS.size() == 0 ?
+                JsonVO.create(null, ResultStatus.FAIL) : JsonVO.create(menuVOS, ResultStatus.SUCCESS);
     }
 
     @Override
@@ -75,7 +88,7 @@ public class SysMenuServiceImpl extends ServiceImpl<SysMenuMapper, SysMenu> impl
             }
         }
 
-        setSortNo(menuDTO, sysMenu);
+        setSortNo(sysMenu);
 
         return save(sysMenu) ? JsonVO.success(ResultStatus.SUCCESS) : JsonVO.fail(ResultStatus.FAIL);
 
@@ -84,12 +97,11 @@ public class SysMenuServiceImpl extends ServiceImpl<SysMenuMapper, SysMenu> impl
     /**
      * 根据menuDTO中的parentId，给sysMenu的sort_no赋值
      *
-     * @param menuDTO
      * @param sysMenu
      */
-    private void setSortNo(MenuDTO menuDTO, SysMenu sysMenu) {
+    private void setSortNo(SysMenu sysMenu) {
         //获取父节点id
-        String parentId = menuDTO.getParentId();
+        String parentId = sysMenu.getParentId();
         if (parentId.equals("0")) {
 
             int temp = 1;
@@ -119,10 +131,10 @@ public class SysMenuServiceImpl extends ServiceImpl<SysMenuMapper, SysMenu> impl
                 sortArray[i] = replace.charAt(i) - '0';
             }
 
+            int temp = 1;
             if (sortArray[1] == 0) {
                 //当父节点的Sort_no小数点后一位为0时，说明父节点为一级菜单，小数点第二位也必定为0，如1.00
                 //temp为小数点后第一位
-                int temp = 1;
                 double sort;
 
                 while (true) {
@@ -138,7 +150,6 @@ public class SysMenuServiceImpl extends ServiceImpl<SysMenuMapper, SysMenu> impl
                 //如果小数点后第一位不为0，即父节点为二级菜单，如1.10；
                 //由于限制只能创建到三级菜单，所以父节点只有三种情况，为空，一级菜单，二级菜单
                 //故父节点sort_no小数点后第二位没有判断的必要
-                int temp = 1;
                 double sort;
 
                 while (true) {
@@ -162,10 +173,10 @@ public class SysMenuServiceImpl extends ServiceImpl<SysMenuMapper, SysMenu> impl
      * @return
      */
     private boolean checking(SysMenu sysMenu, double sort) {
-        QueryWrapper<SysMenu> query = new QueryWrapper<>();
-        query.select("sort_no").eq("sort_no", sort);
-        baseMapper.selectOne(query);
-        if (baseMapper.selectOne(query) == null) {
+        QueryWrapper<SysMenu> queryWrapper = new QueryWrapper<>();
+        queryWrapper.select("sort_no").eq("sort_no", sort);
+        baseMapper.selectOne(queryWrapper);
+        if (baseMapper.selectOne(queryWrapper) == null) {
             //如果没有就将sort值赋给新的menu的sort_no
             sysMenu.setSortNo(sort);
             return true;
@@ -184,14 +195,14 @@ public class SysMenuServiceImpl extends ServiceImpl<SysMenuMapper, SysMenu> impl
             return JsonVO.fail(ResultStatus.FAIL);
         }
         //判断是否在二级菜单下继续新增菜单
-        if (!parentId.equals("0")) {
+        if (!"0".equals(parentId)) {
             double parentSort = query().eq("id", parentId).one().getSortNo();
             String sortString = Double.toString(parentSort);
             if (sortString.length() == 4 && !sortString.endsWith("0")) {
                 return JsonVO.fail(ResultStatus.FAIL);
             }
         }
-        setSortNo(menuDTO, sysMenu);
+        setSortNo(sysMenu);
 
         boolean result = update().eq("id", sysMenu.getId())
                 .set("parent_id", sysMenu.getParentId())
@@ -208,9 +219,68 @@ public class SysMenuServiceImpl extends ServiceImpl<SysMenuMapper, SysMenu> impl
     public JsonVO<ResultStatus> deleteMenu(SysMenuQuery sysMenuQuery) {
 
         SysMenu sysMenu = BeanUtil.copyProperties(sysMenuQuery, SysMenu.class);
-
-        int num = baseMapper.deleteById(sysMenu);
+        //从数据库中获取该对象，可拥有更多属性
+        SysMenu selectFromDB = baseMapper.selectById(sysMenu.getId());
+        //删除id对应的菜单及其所有子节点
+        int num = deleteSub(sysMenu);
+        //对未删除的菜单重新排序
+        resetSortNo(selectFromDB.getSortNo(), selectFromDB.getParentId());
 
         return num >= 1 ? JsonVO.success(ResultStatus.SUCCESS) : JsonVO.fail(ResultStatus.FAIL);
+    }
+
+    /**
+     * deleteMenu中删除id对应菜单及其所有子节点
+     *
+     * @param sysMenu
+     * @return
+     */
+    private int deleteSub(SysMenu sysMenu) {
+        //先将sysMenu中对应id的菜单删除
+        int num = baseMapper.deleteById(sysMenu);
+        //将该节点的子项删除
+        QueryWrapper<SysMenu> querySub = new QueryWrapper<>();
+        querySub.eq("parent_id", sysMenu.getId());
+        List<SysMenu> sysMenus = baseMapper.selectList(querySub);
+        if (!sysMenus.isEmpty()) {
+            for (SysMenu menu : sysMenus) {
+                deleteSub(menu);
+            }
+        }
+        return num;
+    }
+
+    /**
+     * 对未删除的菜单重新排序
+     * @param sortNo
+     * @param parentId
+     */
+    private void resetSortNo(Double sortNo, String parentId) {
+        //获取所有同级菜单
+        QueryWrapper<SysMenu> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("parent_id", parentId);
+        List<SysMenu> sysMenus = baseMapper.selectList(queryWrapper);
+        //若有同级菜单，将同级菜单的sort_no先重置后重新赋值
+        if (!sysMenus.isEmpty()) {
+            //将同级其他节点排序重置
+            for (SysMenu menu : sysMenus) {
+                //只对被删除节点之后的同级节点重置sortNo
+                if (menu.getSortNo() > sortNo) {
+                    menu.setSortNo(0.00);
+                    update().eq("id", menu.getId()).set("sort_no", menu.getSortNo()).update();
+                }
+            }
+            //将同级其他节点重新赋Sort_no
+            List<SysMenu> menus = baseMapper.selectList(queryWrapper);
+            for (SysMenu menu : menus) {
+                //只对被重置后的节点重新赋值
+                if (menu.getSortNo() == 0.00) {
+                    MenuDTO menuDTO = BeanUtil.copyProperties(menu, MenuDTO.class);
+                    updateMenu(menuDTO);
+                    //对每个子节点的子节点组也更新sort_no
+                    resetSortNo(sortNo, menu.getId());
+                }
+            }
+        }
     }
 }
