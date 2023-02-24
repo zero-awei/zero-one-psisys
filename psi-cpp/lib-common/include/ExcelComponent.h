f10b10207d32e9153c708964ea75988ef298ba9a
Jia Hao <863067622@qq.com>
2023-02-24 12:40:22 +0800

    更新了ExcelCompone

--- psi-cpp/lib-common/include/ExcelComponent.h
+++ psi-cpp/lib-common/include/ExcelComponent.h
@@ -24,26 +24,49 @@
 #include <vector>
 /**
  * Excel组件
+ * 注意：xlnt只支持xlsx文件格式
  */
 class ExcelComponent
 {
 private:
 	xlnt::workbook wb;
 	xlnt::worksheet sheet;
+	// 行高
 	double rowHeight = 20;
+	// 列宽
 	double colWidth = 20;
-	//初始化工作薄
-	void initWorkbook(const std::string& fileName = "");
-	//创建Sheet
+	// 创建Sheet
 	void createSheet(const std::string& sheetName);
 public:
-	//设置行高
+	ExcelComponent();
+	// 设置行高
 	void setRowHeight(double rowHeight);
-	//设置列宽
+	// 设置列宽
 	void setColWidth(double colWidth);
-	//读取指定页签内容到容器,注意文件路径使用/
+	// 清空工作薄
+	void clearWorkbook();
+	
+	//************************************
+	// Method:    readIntoVector
+	// FullName:  ExcelComponent::readIntoVector
+	// Access:    public 
+	// Returns:   std::vector<std::vector<std::string>> 指定页签内容的二维vector
+	// Qualifier: 读取指定文件指定页签的内容
+	// Parameter: const std::string & fileName 文件名称的全路径，注意文件路径分隔符使用/
+	// Parameter: const std::string & sheetName 页签名称
+	//************************************
 	std::vector<std::vector<std::string>> readIntoVector(const std::string& fileName, const std::string& sheetName);
-	//保存指定页签内容到文件,注意文件路径使用/
+	
+	//************************************
+	// Method:    writeVectorToFile
+	// FullName:  ExcelComponent::writeVectorToFile
+	// Access:    public 
+	// Returns:   void
+	// Qualifier: 新增内容到指定页签，并保存到文件中
+	// Parameter: const std::string & fileName 文件名称的全路径，注意文件路径分隔符使用/
+	// Parameter: const std::string & sheetName 新增内容保存到的页签名称
+	// Parameter: const std::vector<std::vector<std::string>> & data 新增保存的数据
+	//************************************
 	void writeVectorToFile(const std::string& fileName, const std::string& sheetName, const std::vector<std::vector<std::string>>& data);
 };
 #endif // _EXCELCOMPONENT_H_
