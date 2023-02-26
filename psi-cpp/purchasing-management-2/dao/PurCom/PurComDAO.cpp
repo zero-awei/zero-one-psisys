dd37048ef81a98ad596e3ee34a4cb21715f35870
Janna <Janna@823.com>
2023-02-26 14:38:04 +0800

    PurComDAO已定义

---
+++ psi-cpp/purchasing-management-2/dao/PurCom/PurComDAO.cpp
@@ -0,0 +1,60 @@
+/*
+ Copyright Zero One Star. All rights reserved.
+
+ @Author: awei
+ @Date: 2022/10/25 14:26:52
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+	  https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+#include "stdafx.h"
+#include "PurComDAO.h"
+#include "PurComMApper.h"
+#include <sstream>
+
+//分页查询数据
+list<PurComDO> PurComDAO::selectPurCom(const PurComDO& obj, uint64_t pageIndex, uint64_t pageSize)
+{
+	stringstream sql;
+	// 添加查询语句
+	sql << "SELECT id, bill_no, bill_date, src_bill_type,\
+		src_bill_id, src_no, `subject`, is_rubric,\
+		candidate_quot_ids, payment_method, delivery_place,\
+		delivery_time, attachment, remark, is_auto,\
+		bill_stage, approver, bpmi_instance_id,\
+		approval_result_type, approval_remark,\
+		is_effective, effective_time, is_closed,\
+		is_voided, sys_org_code, create_by, create_time,\
+		update_by, update_time, version FROM pur_compare";
+	SqlParams params;
+
+	// 添加筛选语句
+	sql << " WHERE 1=1";
+	// 筛选条件为单据编号
+	if (!obj.getBill_no().empty()) {
+		sql << " AND `bill_no`=?";
+		params.push_back(SqlParam("s", std::make_shared<std::string>(obj.getBill_no())));
+	}
+	// 筛选条件为单据时间
+	// 
+	// 筛选条件为源单号
+	if (!obj.getSrc_no().empty()) {
+		sql << " AND `src_no`=?";
+		params.push_back(SqlParam("s", std::make_shared<std::string>(obj.getSrc_no())));
+	}
+	// 分页显示
+	sql << " LIMIT " << ((pageIndex - 1) * pageSize) << "," << pageSize;
+	PurComMapper mapper;
+	string sqlStr = sql.str();
+	// 返回查询结果
+	return sqlSession->executeQuery<PurComDO, PurComMapper>(sqlStr, mapper, params);
+}
---
+++ psi-cpp/purchasing-management-2/dao/PurCom/PurComDAO.h
@@ -0,0 +1,39 @@
+#pragma once
+/*
+ Copyright Zero One Star. All rights reserved.
+
+ @Author: qingyu
+ @Date: 2023/2/19 16:41:00
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+	  https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+#ifndef _PUR_COM_DAO_
+#define _PUR_COM_DAO_
+
+#include "BaseDAO.h"
+#include "../../domain/do/PurCom/PurComDO.h"
+
+/*
+*	数据库操作实现 
+*/
+
+
+class PurComDAO : public BaseDAO {
+public:
+	//分页查询数据
+	list<PurComDO> selectPurCom(const PurComDO& obj, uint64_t pageIndex, uint64_t pageSize);
+	//指定单据明细
+	
+};
+
+#endif // ! _PUR_COM_DAO_
