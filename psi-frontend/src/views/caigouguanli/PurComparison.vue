3c9466a45a12873f105767da56a98beefad8fae0
Kyle5 <nnkyle@163.com>
2023-02-27 17:27:36 +0800

    2.27 第一次修改，给每个采购管理添加了弹出框

--- psi-frontend/src/views/caigouguanli/PurComparison.vue
+++ psi-frontend/src/views/caigouguanli/PurComparison.vue
@@ -10,7 +10,7 @@
         @add="addClient">
         <template v-slot:basicOperation="slot">
           <!-- 修改点2 -->
-          <el-button link type="primary" @click="clientEditDialogVisible = true">编辑</el-button>
+          <el-button link type="primary" @click="dialogVisible = true">编辑</el-button>
 
           <el-button link type="primary" @click="deleteRole(slot.data)">删除</el-button>
         </template>
@@ -18,17 +18,179 @@
       </psi-table>
     </div>
 
+    <psi-dialog v-model="dialogVisible" :attrs="attrs">
+      <template #default>
+        <div class="search">
+          <psi-form
+            :items="items"
+            :toggle-items="toggleItems"
+            :formData="formData"
+          ></psi-form>
+        </div>
+
+        <div class="tabs">
+          <el-tabs v-model="activeName" class="demo-tabs">
+            <el-tab-pane label="明细" name="first"> 明细表格</el-tab-pane>
+          </el-tabs>
+        </div>
+
+        <div class="footer">
+          <psi-form
+            :buttonShow="false"
+            :items="footItems"
+            :formData="footFormData"
+          ></psi-form>
+        </div>
+      </template>
+    </psi-dialog>
   </div>
 </template>
     
 <script setup>
 import { reactive, toRefs, ref } from 'vue'
 
+
+// 弹出框
+
+let dialogVisible = ref(false)
+const state = reactive({
+  attrs: {
+    title: '单据',
+    width: '80%'
+  }
+})
+const { attrs } = toRefs(state)
+
+const searchState = reactive({
+  dialogItems: [
+    {
+      type: 'input',
+      label: '单据编号',
+      prop: 'id',
+      placeholder: '请输入单据编号'
+    },
+    {
+      type: 'datePicker',
+      label: '单据日期',
+      prop: 'date',
+      placeholder: ''
+    },
+    {
+      type: 'select',
+      prop: 'do',
+      options: [
+        {
+          label: '执行完',
+          value: 0
+        },
+        {
+          label: '执行中',
+          value: 1
+        }
+      ]
+    },
+    {
+      type: 'select',
+      prop: 'vaildate',
+      label: '已生效',
+      options: [
+        {
+          label: '是',
+          value: 0
+        },
+        {
+          label: '否',
+          value: 1
+        }
+      ]
+    }
+  ],
+  dialogToggleItems: [
+    {
+      type: 'input',
+      label: '单据主题',
+      prop: 'subject',
+      placeholder: '请输入单据主题'
+    },
+    {
+      type: 'select',
+      label: '供应商',
+      prop: 'supplier',
+      placeholder: '请选择',
+      options: [
+        {
+          label: '供应商1',
+          value: 0
+        },
+        {
+          label: '供应商2',
+          value: 1
+        },
+        {
+          label: '供应商3',
+          value: 2
+        }
+      ]
+    },
+    {
+      type: 'input',
+      label: '已核销金额',
+      prop: 'num',
+      placeholder: '已核销金额'
+    }
+  ],
+  dialogFormData: {
+    id: 'QTFY-2022',
+    date: '2023-02-02',
+    do: 0,
+    vaildate: 0,
+    subject: '石化结款',
+    supplier: 0,
+    num: 200
+  }
+})
+// const dialogref = ref()
+// // // console.log('----', dialogref)
+
+let activeName = ref('first')
+
+const footState = reactive({
+  footItems: [
+    {
+      type: 'input',
+      label: '备注',
+      prop: 'note',
+      placeholder: '请输入备注'
+    },
+    {
+      type: 'input',
+      label: '核批意见',
+      prop: 'advice',
+      placeholder: '请输入核批意见'
+    },
+    {
+      type: 'input',
+      label: '核批结果',
+      prop: 'outcome',
+      placeholder: '请输入核批结果'
+    }
+  ],
+  footFormData: {
+    note: '',
+    advice: '',
+    outcome: ''
+  }
+})
+const { dialogItems, dialogToggleItems, dialogFormData } = toRefs(searchState)
+const { footItems, footFormData } = toRefs(footState)
+
+
+
 // 编辑删除
 // 修改点3~5
 function addClient() {
-  addDialogVisible.value = true
-  // addDrawerVisible.value = true
+  dialogVisible.value = true
+
 }
 let clientEditDialogVisible = ref(false)
 
@@ -78,31 +240,151 @@ const status = reactive({
     },
     {
       type: 'text',
-      label: 'Date',
+      label: '单据编号',
       prop: 'date',
       width: '120'
     },
     {
       type: 'text',
-      label: 'City',
+      label: '单据日期',
       prop: 'city',
       width: '120'
     },
     {
       type: 'text',
-      label: 'Address',
+      label: '单据主题',
       prop: 'address',
       width: '600'
     },
     {
       type: 'text',
-      label: 'Zip',
+      label: '源单号',
       prop: 'zip',
       width: '120'
     },
     {
       type: 'text',
-      label: 'Tag',
+      label: '采购类型',
+      prop: 'tag',
+      width: '120'
+    },
+    {
+      type: 'text',
+      label: '需求部门',
+      prop: 'tag',
+      width: '120'
+    },
+    {
+      type: 'text',
+      label: '需求人',
+      prop: 'tag',
+      width: '120'
+    },
+    {
+      type: 'text',
+      label: '需求日期',
+      prop: 'tag',
+      width: '120'
+    },
+    {
+      type: 'text',
+      label: '数量',
+      prop: 'tag',
+      width: '120'
+    },
+    {
+      type: 'text',
+      label: '已订数量',
+      prop: 'tag',
+      width: '120'
+    },
+    {
+      type: 'text',
+      label: '参考金额',
+      prop: 'tag',
+      width: '120'
+    },
+    {
+      type: 'text',
+      label: '单据阶段',
+      prop: 'tag',
+      width: '120'
+    },
+    {
+      type: 'text',
+      label: '已生效',
+      prop: 'tag',
+      width: '120'
+    },
+    {
+      type: 'text',
+      label: '已关闭',
+      prop: 'tag',
+      width: '120'
+    },
+    {
+      type: 'text',
+      label: '已作废',
+      prop: 'tag',
+      width: '120'
+    },
+    {
+      type: 'text',
+      label: '备注',
+      prop: 'tag',
+      width: '120'
+    },
+    {
+      type: 'text',
+      label: '自动单据',
+      prop: 'tag',
+      width: '120'
+    },
+    {
+      type: 'text',
+      label: '红字单据',
+      prop: 'tag',
+      width: '120'
+    },
+    {
+      type: 'text',
+      label: '生效时间',
+      prop: 'tag',
+      width: '120'
+    },
+    {
+      type: 'text',
+      label: '核批人',
+      prop: 'tag',
+      width: '120'
+    },
+    {
+      type: 'text',
+      label: '制单时间',
+      prop: 'tag',
+      width: '120'
+    },
+    {
+      type: 'text',
+      label: '制单人',
+      prop: 'tag',
+      width: '120'
+    },
+    {
+      type: 'text',
+      label: '制单部门',
+      prop: 'tag',
+      width: '120'
+    },
+    {
+      type: 'text',
+      label: '修改时间',
+      prop: 'tag',
+      width: '120'
+    },
+    {
+      type: 'text',
+      label: '修改人',
       prop: 'tag',
       width: '120'
     },
