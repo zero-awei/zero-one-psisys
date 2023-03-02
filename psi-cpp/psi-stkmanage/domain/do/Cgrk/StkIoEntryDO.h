f2eb5593aa85e4a8be857a8e7a35aa7c51b178c4
Jia Hao <863067622@qq.com>
2023-03-02 19:44:20 +0800

    合并

--- psi-cpp/psi-stkmanage/domain/do/Cgrk/StkIoEntryDO.h
+++ psi-cpp/psi-stkmanage/domain/do/Cgrk/StkIoEntryDO.h
@@ -1,7 +1,6 @@
 #pragma once
 /*
  Copyright Zero One Star. All rights reserved.
-<<<<<<< HEAD
 
  @Author: awei
  @Date: 2022/10/25 11:52:32
@@ -12,14 +11,6 @@
 
 	  https://www.apache.org/licenses/LICENSE-2.0
 
-=======
- @Author: awei
- @Date: 2022/10/25 11:52:32
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-	  https://www.apache.org/licenses/LICENSE-2.0
->>>>>>> c5-maobing
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -132,13 +123,7 @@ public:
 		custom1 = "";
 		custom2 = "";
 		version = "";
-
-
-
 	}
-
-
-
 
 };
 
