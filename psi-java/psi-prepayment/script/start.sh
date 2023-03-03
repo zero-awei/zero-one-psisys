012166da75d043ac4dbaba0edc68b9a46922921a
zhd <1106126227@qq.com>
2023-03-03 15:21:23 +0800

    修改脚本

--- psi-java/psi-prepayment/script/start.sh
+++ psi-java/psi-prepayment/script/start.sh
@@ -4,7 +4,7 @@ docker stop ${app_name}
 echo '----stop container----'
 docker rm ${app_name}
 echo '----rm container----'
-docker run -p 10001:10001 --name ${app_name} \
+docker run -p 10690:10690 --name ${app_name} \
 -v /etc/localtime:/etc/localtime \
 -v /home/app/${app_name}/logs:/tmp/logs \
 -d 01star/${app_name}:1.0.0-SNAPSHOT
