86a18ef02d7213c838c45120db1df053d906fec7
160405103 <1348313766@qq.com>
2023-03-03 16:17:56 +0800

    1.需要修改js文件存放位置

---
+++ psi-frontend/src/views/yingfuyufukuan/fukuanguanli/api/caigoutuihuotuikuan.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: 160405103 1348313766@qq.com
+ * @Date: 2023-03-03 16:11:53
+ * @LastEditors: 160405103 1348313766@qq.com
+ * @LastEditTime: 2023-03-03 16:16:47
+ * @FilePath: \psi-frontend\src\views\yingfuyufukuan\fukuanguanli\api\caigoutuihuotuikuan.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+// 采购退货退款（红付）
