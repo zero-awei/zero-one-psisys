1b197d9b1d470466030d63d9fe552d30c44e830a
kiva <3210959990@qq.com>
2023-03-06 20:42:38 +0800

    提交编码bug

--- psi-cpp/psi-c1/api/bankAccountController/BankAccountController.cpp
+++ psi-cpp/psi-c1/api/bankAccountController/BankAccountController.cpp
@@ -1,10 +1,31 @@
 #include "stdafx.h"
 #include "BankAccountController.h"
+#include"CharsetConvertHepler.h"
 #include "../../service/bankAccountService/BankAccountService.h"
 
 JsonVO<PageVO<SpecifiedBankAccountListVO>> BankAccountController::execSpecifiedBankAccount(SpecifiedBankAccountQuery& query)
 {
-    PageVO<SpecifiedBankAccountListVO> data(query.getPageIndex(), query.getPageSize(), 100, 10, { SpecifiedBankAccountListVO() });
+	SpecifiedBankAccountListVO vo;
+	vo.setAccountNo("111");
+	vo.setAttachment("");
+	vo.setBankAddress("");
+	vo.setBankNo("");
+	vo.setCreateBy("psi");
+	vo.setCreateBy_dictText(CharsetConvertHepler::ansiToUtf8("管理员"));
+	vo.setCreateTime("2023-01-14 00:46:24");
+	vo.setCurrency("CNY");
+	vo.setCurrency_dictText(CharsetConvertHepler::ansiToUtf8("人民币"));
+	vo.setId("1625174558616363009");
+	vo.setInitBal(0);
+	vo.setIsEnabled(1);
+	vo.setIsEnabled_dictText(CharsetConvertHepler::ansiToUtf8("是"));
+	vo.setManager("");
+	vo.setName("123");
+	vo.setRemark("");
+	vo.setUpdateBy("");
+	vo.setUpdateTime("");
+	vo.setVersion("");
+	PageVO<SpecifiedBankAccountListVO> data(query.getPageIndex(), query.getPageSize(), 100, 10, { vo });
     JsonVO<PageVO<SpecifiedBankAccountListVO>> res;
     res.success(data);
     return res;
