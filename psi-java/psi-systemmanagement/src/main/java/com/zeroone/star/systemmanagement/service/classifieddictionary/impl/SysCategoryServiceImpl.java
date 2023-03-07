b644e9777785a4335aab833a27d2ad56d2d1010e
wxh <wangxh_6@qq.com>
2023-03-07 14:19:56 +0800

    接口业务修改-3.7

--- psi-java/psi-systemmanagement/src/main/java/com/zeroone/star/systemmanagement/service/classifieddictionary/impl/SysCategoryServiceImpl.java
+++ psi-java/psi-systemmanagement/src/main/java/com/zeroone/star/systemmanagement/service/classifieddictionary/impl/SysCategoryServiceImpl.java
@@ -39,6 +39,11 @@ public class SysCategoryServiceImpl extends ServiceImpl<SysCategoryMapper, SysCa
     @Resource
     private UserHolder holder;
 
+    /**
+     * 查询所有父结点
+     * @param condition
+     * @return
+     */
     @Override
     public PageVO<ClassifiedDictionaryVO> listAll(PageQuery condition) {
         Page<SysCategory> page = new Page<>(condition.getPageIndex(),condition.getPageSize());
@@ -48,6 +53,11 @@ public class SysCategoryServiceImpl extends ServiceImpl<SysCategoryMapper, SysCa
         return PageVO.create(result,ClassifiedDictionaryVO.class);
     }
 
+    /**
+     * 查询对应子节点
+     * @param condition
+     * @return
+     */
     @Override
     public PageVO<ClassifiedDictionaryVO> listChildren(String condition) {
         Page<SysCategory> page = new Page<>(1,10);
@@ -57,6 +67,11 @@ public class SysCategoryServiceImpl extends ServiceImpl<SysCategoryMapper, SysCa
         return PageVO.create(result,ClassifiedDictionaryVO.class);
     }
 
+    /**
+     * 更新对应的子节点
+     * @param data
+     * @return
+     */
     @Override
     public String update(ClassifiedDictionaryUpdateDTO data) {
         if(data.getId() == null){
@@ -80,6 +95,11 @@ public class SysCategoryServiceImpl extends ServiceImpl<SysCategoryMapper, SysCa
         return "更新成功";
     }
 
+    /**
+     * 删除对应的结点
+     * @param id
+     * @return
+     */
     @Override
     public String remove(String id) {
         int result = mapper.deleteById(id);
@@ -89,54 +109,51 @@ public class SysCategoryServiceImpl extends ServiceImpl<SysCategoryMapper, SysCa
         return "删除失败";
     }
 
+    /**
+     * 插入节点
+     * @param data
+     * @return
+     */
     @Override
     public String save(ClassifiedDictionarySaveDTO data) {
         //查询待插入数据是否已存在
         LambdaQueryWrapper<SysCategory> wrapper = new LambdaQueryWrapper<>();
         wrapper.eq(SysCategory::getName,data.getName()).eq(SysCategory::getPid,data.getPid());
-        SysCategory selectOne = mapper.selectOne(wrapper);
-        if(selectOne != null){
+        List<SysCategory> sysCategories = mapper.selectList(wrapper);
+        if(sysCategories.size() != 0){
             return "数据已存在";
-        }
-        //创建date对象获取当前时间
-        LocalDateTime now = LocalDateTime.now();
-        SysCategory sysCategory = new SysCategory();
-        //对象属性匹配
-        BeanUtil.copyProperties(data,sysCategory);
-        //判断是否有子节点
-        LambdaQueryWrapper<SysCategory> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(SysCategory::getPid,data.getPid());
-        List<SysCategory> list = mapper.selectList(queryWrapper);
-        int records = list.size();
-
-        //根据父节点查子节点的数量为0
-        if(records == 0 || data.getPid().equals("0")){
-            sysCategory.setCode("A01");
+        }else {
+            SysCategory sysCategory = new SysCategory();
+            //对象属性匹配
+            BeanUtil.copyProperties(data,sysCategory);
+            //获取并排序同级结点
+            List<SysCategory> list = mapper.selectNodeOrderByCreateTime(data.getPid());
+            int size = list.size();
+            //判断pid==0并判断一级结点的数量是否为0
+            if(data.getPid().equals("0") && size == 0){
+                sysCategory.setCode("A01");
+            }else if(size == 0){        //判断子节点的数量是否为0
+                LambdaQueryWrapper<SysCategory> queryWrapper = new LambdaQueryWrapper<>();
+                queryWrapper.eq(SysCategory::getId,data.getPid());
+                SysCategory parentCategory = mapper.selectOne(queryWrapper);
+                sysCategory.setCode(parentCategory.getCode()+"A01");
+                //更新父节点HasChild字段
+                parentCategory.setHasChild("1");
+                makeOperator(parentCategory);
+                mapper.updateById(parentCategory);
+            }else {
+                String code = makeCode(list);
+                sysCategory.setCode(code);
+            }
             sysCategory.setHasChild("0");
+            //set操作人和更新人
+            String resultOperator = makeOperator(sysCategory);
+            int result = mapper.insert(sysCategory);
+            if(result != 0 && resultOperator.equals("1")){
+                return "添加成功";
+            }
+            return "添加失败";
         }
-        //根据父节点查子节点的数量不为0
-        sysCategory.setHasChild("1");
-        //实现sql，查询出所有子节点并按照创建时间降序获取top1
-        List<SysCategory> listLastNode =mapper.selectNodeOrderByCreateTime(data.getPid());
-        String code = makeCode(listLastNode);
-        if(data.getPid().equals("0")){
-            sysCategory.setCode(code);
-        }
-        sysCategory.setCode(data.getPid()+code);
-        //set操作人和更新人
-        try{
-           UserDTO userDTO = holder.getCurrentUser();
-           sysCategory.setCreateBy(userDTO.getUsername());
-           sysCategory.setUpdateBy(userDTO.getUsername());
-        }catch (Exception e){
-            return e.getMessage();
-        }
-        int result = mapper.insert(sysCategory);
-        System.out.println("test4");
-        if(result != 0){
-            return "添加成功";
-        }
-        return "添加失败";
     }
 
 
@@ -146,21 +163,45 @@ public class SysCategoryServiceImpl extends ServiceImpl<SysCategoryMapper, SysCa
      * @return code节点同级编码
      */
     public String makeCode(List<SysCategory> list){
+        //获取已经根据创建时间排序好的list的数量
         int recodes = list.size();
-        String result = null;
+        //创建拼接好code的变量
+        String result = "";
+        //获取最后一个创建的同阶结点
         SysCategory sysCategory = list.get(0);
+        //将该节点的编码字段变成一个字符数组
         char[] chars = sysCategory.getCode().toCharArray();
+        //判断同级结点的数量是否超过到9这个临界点
         if(recodes%10 == 9){
+            //如果到9这个临界点则倒数第二个字符+1，最后一个字符为0
             chars[chars.length-1] = 0;
             chars[chars.length-2] = ++chars[chars.length-2];
+            //将设置好的字符数组转换为字符串
             result = String.valueOf(chars);
             return result;
         }else {
+            //如果没有到9这个临界点数组的最后一个字符自增
             chars[chars.length-1] = ++chars[chars.length-1];
             result = String.valueOf(chars);
+            return result;
         }
-        return result;
     }
+
+    /**
+     * 更新操作人信息
+     * @param sysCategory
+     * @return
+     */
+  public String makeOperator(SysCategory sysCategory){
+      try{
+          UserDTO userDTO = holder.getCurrentUser();
+          sysCategory.setCreateBy(userDTO.getUsername());
+          sysCategory.setUpdateBy(userDTO.getUsername());
+      }catch (Exception e){
+          return e.getMessage();
+      }
+      return "1";
+  }
 }
 
 
