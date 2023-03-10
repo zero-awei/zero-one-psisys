0783272b04901a7512b64302c68fff5b6adb9c70
wxh <wangxh_6@qq.com>
2023-03-10 11:45:43 +0800

    删除冗余

--- psi-java/psi-titlepage/src/main/java/com/zeroone/star/titlepage/TitlePageApp.java
+++ psi-java/psi-titlepage/src/main/java/com/zeroone/star/titlepage/TitlePageApp.java
@@ -1,16 +1,9 @@
 package com.zeroone.star.titlepage;
 
-import com.alibaba.nacos.api.exception.NacosException;
-import com.alibaba.nacos.api.naming.NamingFactory;
-import com.alibaba.nacos.api.naming.NamingService;
-import com.alibaba.nacos.api.naming.pojo.Instance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.Properties;
 
 /**
  * @Description
@@ -20,18 +13,8 @@ import java.util.Properties;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class TitlePageApp {
-    public static void main(String[] args) throws NacosException, UnknownHostException {
-       /* Properties properties = new Properties();
-        properties.setProperty("namespace", "7fc50d88-3410-44a3-acff-9108d2bfe2b2");
-        properties.setProperty("serverAddr","43.138.51.248:8848");
-        NamingService namingService = NamingFactory.createNamingService(properties);
-        // 创建服务实例
-        Instance instance = new Instance();
-        instance.setIp(InetAddress.getLocalHost().getHostAddress());
-        instance.setPort(10102);
-        instance.setServiceName("homepage-j2");
-        // 注册服务实例到 Nacos 服务注册中心
-        namingService.registerInstance(instance.getServiceName(), instance.getIp(), instance.getPort());*/
+    public static void main(String[] args){
+
         SpringApplication.run(TitlePageApp.class,args);
     }
 }
